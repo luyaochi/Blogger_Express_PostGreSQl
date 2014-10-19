ee2563b11efbcac3206ae43b46c9cbf001a1ede2
luyaochi <ab456ab789g@gmail.com>
2014-10-20 02:09:15 +0800

    fix error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.render('index.ejs');
+  response.render('index.ejs',{'data':'testing data'});
 })
 
 app.get('/db', function (request, response) {
