418412d68c964ba3316aed90569c5533354d3cfd
luyaochi <ab456ab789g@gmail.com>
2014-10-20 06:57:51 +0800

    delete double listen port

--- index.js
+++ index.js
@@ -102,8 +102,3 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
 
-
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
