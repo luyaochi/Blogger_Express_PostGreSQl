d96707880d2e4aa0643f37af577a575d998f6ae2
luyaochi <ab456ab789g@gmail.com>
2014-10-20 22:48:47 +0800

    add db2.ejs

--- index.js
+++ index.js
@@ -28,7 +28,20 @@ app.get('/db', function (request, response) {
     });
   });
 })
-
+app.get('/db2', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM blogger_table;', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { 
+        var a = result.rows;
+        response.render('db.ejs',{'data' : a}); 
+      }
+    });
+  });
+})
 
 app.post('/db/create', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
