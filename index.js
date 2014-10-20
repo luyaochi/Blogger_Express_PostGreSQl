b4aeaf669a50b1e3921cbf778b33ec0ad3267ea1
luyaochi <ab456ab789g@gmail.com>
2014-10-20 23:18:07 +0800

    add decoderjson

--- index.js
+++ index.js
@@ -49,8 +49,7 @@ app.get('/db2', function (request, response) {
 app.post('/db/insert', function (request, response) {
   var a = request.body;
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    //insert into blogger_table  values (1, \"hello database\");
-    client.query('SELECT * FROM blogger_table;', function(err, result) {
+    client.query('insert into blogger_table  values (' + a + ');', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
